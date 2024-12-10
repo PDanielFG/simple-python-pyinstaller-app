ee24175defdf0b729910a648dfed866437dc3bed
daniel <pedrodaniel.fernandezguerrero@alum.uca.es>
2024-12-10 17:27:55 +0100

    archivos creados. Funciona terraform

---
+++ main.tf
@@ -0,0 +1,93 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.0"
+    }
+  }
+  required_version = ">= 1.0"
+}
+
+provider "docker" {}
+
+# Crear una red para Jenkins
+resource "docker_network" "jenkins" {
+  name = "jenkins"
+}
+
+# Crear volúmenes para Jenkins
+resource "docker_volume" "jenkins_data" {
+  name = "jenkins-data"
+}
+
+resource "docker_volume" "jenkins_certs" {
+  name = "jenkins-docker-certs"
+}
+
+# Contenedor Docker in Docker
+resource "docker_container" "jenkins_docker" {
+  name  = "jenkins-docker"
+  image = "docker:dind"
+  privileged = true
+
+  networks_advanced {
+    name = docker_network.jenkins.name
+  }
+
+  volumes {
+    volume_name    = docker_volume.jenkins_data.name
+    container_path = "/var/jenkins_home"
+  }
+
+  volumes {
+    volume_name    = docker_volume.jenkins_certs.name
+    container_path = "/certs/client"
+  }
+
+  ports {
+    internal = 2376
+    external = 2376
+  }
+
+  env = [
+    "DOCKER_TLS_CERTDIR=/certs",
+  ]
+}
+
+# Contenedor Jenkins con Blue Ocean
+resource "docker_container" "jenkins_blueocean" {
+  name  = "jenkins-blueocean"
+  image = "myjenkins-blueocean"
+  restart = "on-failure"
+
+  networks_advanced {
+    name = docker_network.jenkins.name
+  }
+
+  ports {
+    internal = 8080
+    external = 8080
+  }
+
+  ports {
+    internal = 50000
+    external = 50000
+  }
+
+  env = [
+    "DOCKER_HOST=tcp://docker:2376",
+    "DOCKER_CERT_PATH=/certs/client",
+    "DOCKER_TLS_VERIFY=1",
+  ]
+
+  volumes {
+    volume_name    = docker_volume.jenkins_data.name
+    container_path = "/var/jenkins_home"
+  }
+
+  volumes {
+    volume_name    = docker_volume.jenkins_certs.name
+    container_path = "/certs/client"
+    read_only      = true
+  }
+}
